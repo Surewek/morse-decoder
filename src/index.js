8857176fe7b6b56800bd2a65b75c05a7b628c7c6
Vitaly Vegera <surewek@gmail.com>
2022-02-28 20:08:49 +0300

    feat: complite morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let rawArr = expr.split('');
+
+    let binaryArr = [];
+    let letterArr = [];
+    let subarrSize = 10;
+    let letterCode;
+
+    for(let i = 0; i < rawArr.length / subarrSize; i++){
+      letterCode = [];
+      binaryArr[i] = rawArr.slice(i * subarrSize, (i * subarrSize) + subarrSize);
+
+      for(let j = 0; j < binaryArr[i].length / 2; j++){
+        if((binaryArr[i][j * 2] == 0) && (binaryArr[i][(j * 2) + 1] == 0)){
+          continue;
+        }
+        if(binaryArr[i][j * 2] == 1 && binaryArr[i][(j * 2) + 1] == 0){
+          letterCode.push('.');          
+        }
+        if(binaryArr[i][j * 2] == 1 && binaryArr[i][(j * 2) + 1] == 1){
+          letterCode.push('-');
+        }
+      }
+      letterArr.push(MORSE_TABLE[letterCode.join('')] || ' ');
+    }
+    return letterArr.join('');
 }
 
 module.exports = {
